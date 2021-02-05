d3bc3692b37863ea95070846d84a3e0e362aed98
Navin <navinchinnasamy@gmail.com>
2021-02-06 01:19:49 +0530

    Added Axios and React Router

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './logo.svg';
 import './App.css';
 
+import AddExpense from './components/add-expense.component.js';
+import Expense from './components/expense.component.js';
+import ExpenseList from './components/expense-list.component.js';
+
 class App extends Component {
   render(){
 	return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+		<a href="/expenses" className="navbar-brand">
+			Navin
+		</a>
+		<div className="navbar-nav mr-auto">
+			<li className="nav-item">
+				<Link to={"/expenses"} className="nav-link">Expenses</Link>
+			</li>
+			<li className="nav-item">
+				<Link to={"/add"} className="nav-link">Add</Link>
+			</li>
+		</div>
+	  </nav>
+	  
+	  <div className="container mt-3">
+		<Switch>
+			<Route exact path={["/", "/expenses"]} component={ExpenseList} />
+			<Route exact path="/add" component={AddExpense} />
+			<Route exact path="/expenses/:id" component={Expense} />
+		</Switch>
+	  </div>
     </div>
     );
   }
